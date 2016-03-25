80b34068d74f4cf23e1175a6f7defd411cf99ec5
zimoushou <739844323@qq.com>
2016-03-25 15:46:59 +0800

    update

--- js/test_16.js
+++ js/test_16.js
@@ -20,33 +20,33 @@ var flag_value = false;
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
-	if(flag_name&&flag_value&&!aqiData[dom_name.value]){
-		aqiData[dom_name.value] = dom_value.value;	
-		tempData[dom_name.value] = dom_value.value;	
-		// apiArray.push(dom_name.value);
-	}
+  if(flag_name&&flag_value&&!aqiData[dom_name.value]){
+    aqiData[dom_name.value] = dom_value.value;  
+    tempData[dom_name.value] = dom_value.value; 
+    // apiArray.push(dom_name.value);
+  }
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList(_data,_type) {
-	for(var aqi in _data){
-		var _dom_tr = dom_tr.cloneNode();
-		var td_name = document.createElement("td");
-		var td_value = document.createElement("td");
-		var td_btn = document.createElement("td");
-		var td_btn_b = document.createElement("button");
-		td_name.innerHTML = aqi;
-		td_value.innerHTML = _data[aqi];
-		td_btn_b.innerHTML = "删除";
-		td_btn_b.className = "btn btn-primary";
-		td_btn.appendChild(td_btn_b);
-		_dom_tr.appendChild(td_name);
-		_dom_tr.appendChild(td_value);
-		_dom_tr.appendChild(td_btn);
-		dom_table.appendChild(_dom_tr);
-	}
+  for(var aqi in _data){
+    var _dom_tr = dom_tr.cloneNode();
+    var td_name = document.createElement("td");
+    var td_value = document.createElement("td");
+    var td_btn = document.createElement("td");
+    var td_btn_b = document.createElement("button");
+    td_name.innerHTML = aqi;
+    td_value.innerHTML = _data[aqi];
+    td_btn_b.innerHTML = "删除";
+    td_btn_b.className = "btn btn-primary";
+    td_btn.appendChild(td_btn_b);
+    _dom_tr.appendChild(td_name);
+    _dom_tr.appendChild(td_value);
+    _dom_tr.appendChild(td_btn);
+    dom_table.appendChild(_dom_tr);
+  }
 }
 
 /**
@@ -80,31 +80,35 @@ function init() {
   // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
   dom_btn.onclick = addBtnHandle;
   dom_table.addEventListener("click",function(e){
-  		if(e.target&&e.target.nodeName =="BUTTON"){
-  				delBtnHandle(e.target.parentNode.parentNode);
-  		}
+      if(e.target&&e.target.nodeName =="BUTTON"){
+          delBtnHandle(e.target.parentNode.parentNode);
+      }
   });
   dom_name.addEventListener("blur",function(e){
-  		var val_name = e.target.value;
-  		var dom = e.target;
-  		if(/^([\u4e00-\u9fa5]|[A-z])+$/.test(val_name)){
-			flag_name = true;
-			dom.className = "form-control"
-  		}else{
-  			flag_name = false;
-  			dom.className = "form-control wp-bd"
-  		}
+      var val_name = e.target.value;
+      val_name = val_name.replace(/\s|\n|\f|\t|\v|\r/g, "");
+      e.target.value = val_name;
+      var dom = e.target;
+      if(/^([\u4e00-\u9fa5]|[A-z])+$/.test(val_name)){
+      flag_name = true;
+      dom.className = "form-control"
+      }else{
+        flag_name = false;
+        dom.className = "form-control wp-bd"
+      }
   });
   dom_value.addEventListener("blur",function(e){
-		var val_value = e.target.value;
-		var dom = e.target;
-  		if(/^[1-9][0-9]*$/.test(val_value)){
-			flag_value = true;
-			dom.className = "form-control"
-  		}else{
-  			flag_value = false;
-			dom.className = "form-control wp-bd"
-  		}
+    var val_value = e.target.value;
+      val_value = val_value.replace(/\s|\n|\f|\t|\v|\r/g, "");
+      e.target.value = val_value;
+    var dom = e.target;
+      if(/^[1-9][0-9]*$/.test(val_value)){
+      flag_value = true;
+      dom.className = "form-control"
+      }else{
+        flag_value = false;
+      dom.className = "form-control wp-bd"
+      }
   });
 }
 
